d9a82fbabe6cc14e2e6edeb274d8ecf298a5792a
sKawashima <bysk.333@gmail.com>
2024-06-01 22:23:49 +0900

    ✨ (pdf2json/src/index.ts): enhance PDF text extraction to include page-wise text aggregation and filtering of empty strings for cleaner output

--- pdf2json/src/index.ts
+++ pdf2json/src/index.ts
@@ -4,9 +4,24 @@ import PDFParser from 'pdf2json';
 const pdfParser = new PDFParser();
 
 pdfParser.on("pdfParser_dataReady", (pdfData) => {
+  const pagesText = pdfData.Pages.map(page => {
+    return page.Texts.map(text => {
+      return text.R.map(r => {
+        const decodedText = decodeURIComponent(r.T);
+        return decodedText;
+      }).filter((text) => text !== '').join('');
+    });
+  });
+
   fs.writeFile(
     "./output.json",
-    decodeURI(JSON.stringify(pdfData)),
+    JSON.stringify(pagesText.map((texts, pageIndex) => {
+      return {
+        page: pageIndex + 1,
+        texts: texts
+      };
+    })
+  ),
     (data) => console.log(data)
   );
 });
